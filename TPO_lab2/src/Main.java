22e845ca06def053db6ff344832dca66c393c15a
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-04-18 13:31:57 +0300

    .

--- TPO_lab2/src/Main.java
+++ TPO_lab2/src/Main.java
@@ -1,5 +1,7 @@
+import function.primitive.Ln;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Ln.calc(0, 100));
     }
 }
--- TPO_lab2/src/function/primitive/Ln.java
+++ TPO_lab2/src/function/primitive/Ln.java
@@ -1,15 +1,32 @@
 package function.primitive;
 
 public class Ln {
-    public static double[] derivativesAtZero(int order) {
+    public static double[] derivativesAtOne(int order) {
         double[] derivatives = new double[order + 1];
         double factorial = 1;
 
-        for (int i = 0; i < order; i++) {
-            derivatives[i] = derivatives_at_zero_values[i & 3];
-            factorial *= i + 1;
+        derivatives[0] = 0;
+
+        for (int i = 1; i < order; i++) {
+            derivatives[i] = factorial;
+            factorial *= -i;
         }
 
         return derivatives;
+    }
+
+    public static double calc(double x, int order) {
+        x -= 1;
+        double result = 0;
+        double x_power = x;
+        double sign = 1;
+
+        for (int i = 1; i <= order; i++) {
+            result += sign * x_power / i;
+            sign = -sign;
+            x_power *= x;
+        }
+
+        return result;
     }
 }
