8c66b4feb8fbcab577211a7c72d3052b9280e21a
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-04-18 23:48:37 +0300

    .

--- TPO_lab2/test/src/CustomTest.java
+++ TPO_lab2/test/src/CustomTest.java
@@ -10,8 +10,8 @@ public class CustomTest {
     private final double accuracy = 0.0001;
 
     @ParameterizedTest(name = "custom({0}) = {1}")
-    @DisplayName("Проверки между (-pi/2; +pi/2)")
-    @CsvFileSource(resources = "/csv/custom.csv", numLinesToSkip = 1, delimiter = ',')
+    @DisplayName("Что-то")
+    @CsvFileSource(resources = "/csv/expected/custom.csv", numLinesToSkip = 1, delimiter = ',')
     public void checkBetweenPI(double x, double custom) {
         assertAll(
                 () -> assertEquals(custom, new Custom().calc(x, accuracy), accuracy)
